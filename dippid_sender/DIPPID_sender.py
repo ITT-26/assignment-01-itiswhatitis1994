180b82a109562d4f3b8abd161f5130dc947ebfbf
itiswhatitis1994 <Vanessa.Brandwirth@stud.uni-regensburg.de>
2026-04-25 15:42:13 +0200

    Task 1: added documentation

--- dippid_sender/DIPPID_sender.py
+++ dippid_sender/DIPPID_sender.py
@@ -12,23 +12,27 @@ counter=0
 
 while True:
     
+    # this creates the message that is used for the button
     message_but = {"button_1" : str(counter)}
+    # 0 or 1 is what is sent to show if button pressed or released
     if (counter == 0):
         counter=1
     else:
         counter=0
-
     final_message_but = json.dumps(message_but)
-
+    #sends button message
     sock.sendto(final_message_but.encode(), (IP, PORT))
 
+    # creates accelerometer data
     x = math.sin(time.time())
     y = math.sin(-3* time.time())
     z = math.sin(4*time.time())
     
+    # turns data into a message
     message_acc = {"accelerometer": {'X': x, 'Y': y, 'Z': z}}
     final_message_acc = json.dumps(message_acc)
 
+    # sends accerlerometer data
     sock.sendto(final_message_acc.encode(), (IP, PORT))
     
     time.sleep(1)
